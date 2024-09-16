4495118d34e465e3cfefa79f6cb8e96381e91007
Varunbansalchrgfg <varunbansaldcsamca@gmail.com>
2024-09-17 00:36:40 +0530

    Disable_output_processing_by_the_terminal.

--- text_editor.cpp
+++ text_editor.cpp
@@ -1,44 +1,53 @@
-#include<iostream>
-#include<termios.h>
-#include<unistd.h>
-class UnbufferedInput{
-    public:
-    //Constructor
-    UnbufferedInput(){
-        //get the current terminal settings and store into orig_termios 
-        tcgetattr(STDIN_FILENO,&orig_termios);
-        //Now copy those values into new termios
-        newtermios=orig_termios;
-        //Manipulate those values.
-        newtermios.c_lflag&=~(ICANON|ECHO);
-        tcsetattr(STDIN_FILENO,TCSANOW,&newtermios);
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+class UnbufferedInput
+{
+public:
+    // Constructor
+    UnbufferedInput()
+    {
+        // get the current terminal settings and store into orig_termios
+        tcgetattr(STDIN_FILENO, &orig_termios);
+        // Now copy those values into new termios
+        newtermios = orig_termios;
+        // Manipulate those values.
+        newtermios.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ISIG);
+        newtermios.c_lflag &= ~(OPOST);
+        newtermios.c_lflag &= ~(ICRNL | IXON | INPCK | ISTRIP);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);
     }
-    //Destructor to make the terminal to original state.
-    ~UnbufferedInput(){
-        tcsetattr(STDIN_FILENO,TCSANOW,&orig_termios);
+    // Destructor to make the terminal to original state.
+    ~UnbufferedInput()
+    {
+        tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
     }
     // Getchar functiion to get the value and retun the value
-    char getChar(){
+    char getChar()
+    {
         char c;
-        //STDIN_FILE_NO:- reads the single character from the keyboard
-        read(STDIN_FILENO,&c,1);
+        // STDIN_FILE_NO:- reads the single character from the keyboard
+        read(STDIN_FILENO, &c, 1);
         return c;
     }
-    private:
-    struct termios newtermios,orig_termios;
+
+private:
+    struct termios newtermios, orig_termios;
 };
-int main(){
+int main()
+{
     // Creates the object of the class
     UnbufferedInput input;
     char c;
-    std::cout<<"Press any key (OR Press Q to exit)"<<std::endl;
+    std::cout << "Press any key (OR Press Q to exit)" << std::endl;
     // Get the value from the user until the user press the q button to quit.
-    while(true){
-        //Get input from the user
-        c=input.getChar();
-        std::cout<<"You Pressed Key "<<c<<std::endl;
-        if(c=='q')
-        break;
+    while (true)
+    {
+        // Get input from the user
+        c = input.getChar();
+        std::cout << "You Pressed Key " << c << std::endl;
+        if (c == 'q')
+            break;
     }
     return 0;
 }
