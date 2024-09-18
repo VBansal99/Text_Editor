6be17216bad3df7469d4a7eeb09ae5a6437eef1a
Varunbansalchrgfg <varunbansaldcsamca@gmail.com>
2024-09-19 00:49:35 +0530

    Control_and_editor_mode_implementation

--- text_editor.cpp
+++ text_editor.cpp
@@ -2,6 +2,8 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <vector>
+#include <string>
 
 class UnbufferedInput
 {
@@ -22,7 +24,7 @@ public:
         newtermios.c_lflag &= ~(OPOST);
         newtermios.c_lflag &= ~(ICRNL | IXON | INPCK | ISTRIP);
         tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);
-        drawverticallines('-');
+        drawverticallines('~');
     }
     // Destructor to make the terminal to original state.
     ~UnbufferedInput()
@@ -34,68 +36,128 @@ public:
         tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
     }
     // Getchar functiion to get the value and retun the value
-    char getChar(){
+    char getChar()
+    {
         char c;
         // STDIN_FILE_NO:- reads the single character from the keyboard
         read(STDIN_FILENO, &c, 1);
         return c;
     }
-        void movecursorup(int n=1){
-            std::cout << "\033[" << n << "A";
-        }
-        void movecursordown(int n=1){
-            std::cout << "\033[" << n << "B";
-        }
-        void movecursorleft(int n=1){
-            std::cout << "\033[" << n << "D";
-        }
-        void movecursorright(int n=1){
-            std::cout << "\033[" << n << "C";
-        }
+    void enableecho(){
+        newtermios.c_lflag |=ECHO;
+        tcsetattr(STDIN_FILENO,TCSANOW, &newtermios);
+    }
+    void disableecho(){
+        newtermios.c_lflag &=~ECHO;
+        tcsetattr(STDIN_FILENO,TCSANOW, &newtermios);
+    }
+    void movecursorup(int n = 1)
+    {
+        std::cout << "\033[" << n << "A";
+    }
+    void movecursordown(int n = 1)
+    {
+        std::cout << "\033[" << n << "B";
+    }
+    void movecursorleft(int n = 1)
+    {
+        std::cout << "\033[" << n << "D";
+    }
+    void movecursorright(int n = 1)
+    {
+        std::cout << "\033[" << n << "C";
+    }
 
 private:
     struct termios newtermios, orig_termios;
 
-    void drawverticallines(char c){
+    void drawverticallines(char c)
+    {
         int row_size = 0;
         int col_size = 0;
         struct winsize w;
-        if (ioctl(STDIN_FILENO, TIOCGWINSZ, &w) == -1){
+        if (ioctl(STDIN_FILENO, TIOCGWINSZ, &w) == -1)
+        {
             perror("Invalid window size");
             return;
         }
-        else{
-            row_size = w.ws_row;
-            col_size = w.ws_col;
-        }
-        for (int i = 1; i < row_size; i++){
+        
+        row_size = w.ws_row;
+        for (int i = 0; i < row_size; i++)
+        {
             std::cout << c << std::endl;
         }
-        std::cout << "\033[1;2H";
+        std::cout << "\033[1;1H";
     }
 };
-int main(){
+int main()
+{
     // Creates the object of the class
     UnbufferedInput input;
     char c;
     std::cout << "Press any key (OR Press Q to exit)" << std::endl;
     // Get the value from the user until the user press the q button to quit.
+    std::string currentline;
+    std::vector<std::string> data;
+    bool isEditorMode = false;
     while (true)
     {
-        // Get input from the user
         c = input.getChar();
-        if (c == 'q')
-            break;
-        if(c=='w')
-        input.movecursorup();
-        else if(c=='a')
-        input.movecursorleft();
-        else if(c=='s')
-        input.movecursordown();
-        else if(c=='d')
-        input.movecursorright();
+        if (isEditorMode)
+        { // Esc Key is pressed
+            if (c == 27)
+            {
+                c = input.getChar();
+                if (c == 'i'){
+                std::cout<<std::endl;
+                    std::cout << "User is in editor mode" << std::endl;
+                }
+                else
+                {
+                    isEditorMode = false;
+                    input.disableecho();
+                    std::cout << "User is in control mode" << std::endl;
+                }
+            }
+            else if (c == '\n')
+            {
+                data.push_back(currentline);
+                currentline.clear();
+            }
+            else
+            {
+                currentline += c;
+            }
+        }
         else
-        std::cout << "You Pressed Key " << c << std::endl;
+        { // Control Mode
+            if (c == 27)
+            {
+                c = input.getChar();
+                if (c == 'i')
+                {
+                    isEditorMode = true;
+                    input.enableecho();
+                    std::cout << "Switched to editor mode and type your text" << std::endl;
+                }
+            }
+            else
+            {
+                if (c == 'q')
+                    break; // Exit command
+                // Handle other control commands here
+                if (c == 'w')
+                    input.movecursorup();
+                else if (c == 'a')
+                    input.movecursorleft();
+                else if (c == 's')
+                    input.movecursordown();
+                else if (c == 'd')
+                    input.movecursorright();
+                else
+                    std::cout << "Unknown command: " << c << std::endl;
+            }
+        }
     }
     return 0;
 }
