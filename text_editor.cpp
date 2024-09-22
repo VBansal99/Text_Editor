63a1a789ab56f7ea34cfcd275c8a770f6e661dab
Varunbansalchrgfg <varunbansaldcsamca@gmail.com>
2024-09-22 19:49:32 +0530

    Refine the code

--- text_editor.cpp
+++ text_editor.cpp
@@ -6,13 +6,13 @@
 #include <string>
 #include <fstream>
 
-class UnbufferedInput
+class terminalSettings
 {
 public:
-    UnbufferedInput()
+    terminalSettings()
     {
         std::cout << "\033[2J";
-        std::cout << "\033[1;5H";
+        std::cout << "\033[1;1H";
         tcgetattr(STDIN_FILENO, &orig_termios);
         newtermios = orig_termios;
         newtermios.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ISIG);
@@ -20,44 +20,49 @@ public:
         newtermios.c_lflag &= ~(ICRNL | IXON | INPCK | ISTRIP);
         newtermios.c_iflag &= ~(IXON);
         tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);
-        //drawverticallines('~');
     }
-    ~UnbufferedInput()
+
+    ~terminalSettings()
     {
         std::cout << "\033[2J";
         std::cout << "\033[1;1H";
         tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
     }
+
     char getChar()
     {
         char c;
         read(STDIN_FILENO, &c, 1);
         return c;
     }
+
     void enableecho()
     {
         newtermios.c_lflag |= ECHO;
-        // newtermios.c_lflag |= (ICRNL | IXON | INPCK | ISTRIP);
         tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);
     }
+
     void disableecho()
     {
         newtermios.c_lflag &= ~ECHO;
-        // newtermios.c_lflag &= ~(ICRNL | IXON | INPCK | ISTRIP);
         tcsetattr(STDIN_FILENO, TCSANOW, &newtermios);
     }
+
     void movecursorup(int n = 1)
     {
         std::cout << "\033[" << n << "A";
     }
+
     void movecursordown(int n = 1)
     {
         std::cout << "\033[" << n << "B";
     }
+
     void movecursorleft(int n = 1)
     {
         std::cout << "\033[" << n << "D";
     }
+
     void movecursorright(int n = 1)
     {
         std::cout << "\033[" << n << "C";
@@ -65,40 +70,43 @@ public:
 
 private:
     struct termios newtermios, orig_termios;
-
-    void drawverticallines(char c)
-    {
-        int row_size = 0;
-        int col_size = 0;
-        struct winsize w;
-        if (ioctl(STDIN_FILENO, TIOCGWINSZ, &w) == -1)
-        {
-            perror("Invalid window size");
-            return;
-        }
-
-        row_size = w.ws_row;
-        for (int i = 0; i < row_size; i++)
-        {
-            std::cout << c << std::endl;
-        }
-        std::cout << "\033[1;1H";
-    }
 };
+void saveFile(std::string filename, std::vector<std::string>data)
+    {
+        std::cout << std::endl;
+        std::cout << "Enter the file name:- " << std::endl;
+        std::cin >> filename;
+        std::ofstream saveFile(filename);
+        if (saveFile.is_open())
+        {
+            for (const auto &line : data)
+            {
+                saveFile << line << std::endl;
+            }
+            saveFile.flush();
+            saveFile.close();
+            std::cout << filename << " saved successfully" << std::endl;
+        }
+        else
+        {
+            std::cerr << "Failed to save file" << std::endl;
+        }
+    }
 int main()
 {
-    UnbufferedInput input;
+    terminalSettings input;
     char c;
     std::cout << "Press any key (OR Press Q to exit)" << std::endl;
     std::string currentline;
     std::vector<std::string> data;
     bool isEditorMode = false;
     std::string filename;
+
     while (true)
     {
         c = input.getChar();
         if (isEditorMode)
-        { 
+        {
             if (c == 27)
             {
                 c = input.getChar();
@@ -118,23 +126,19 @@ int main()
             {
                 data.push_back(currentline);
                 currentline.clear();
+                std::cout << std::endl;
             }
             else if (c == 19)
             {
-                std::cout << "Enter the file name:- " << std::endl;
-                std::cin >> filename;
-                std::ofstream saveFile(filename);
-                if (saveFile.is_open())
+                saveFile(filename, data);
+            }
+            else if (c == 127 || c == 8)
+            {
+                if (!currentline.empty())
                 {
-                    for (const auto &line : data)
-                    {
-                        saveFile << line << std::endl;
-                    }
-                    saveFile.close();
-                    std::cout << filename << "saved successfully" << std::endl;
+                    currentline.pop_back();
+                    std::cout << "\b \b";
                 }
-                else
-                    std::cerr << "Failed to save file" << std::endl;
             }
             else
             {
@@ -150,21 +154,20 @@ int main()
                 {
                     isEditorMode = true;
                     input.enableecho();
-                    std::cout << "Switched to editor mode and type your text" << std::endl;
+                    std::cout << "Switched to editor mode. Type your text." << std::endl;
                 }
             }
             else
             {
-                if (c == 'q')
-                    break; // Exit command
-                // Handle other control commands here
-                if (c == 'w')
+                if (c == 'q' || c == 'Q')
+                    break;
+                if (c == 'w' || c == 'W')
                     input.movecursorup();
-                else if (c == 'a')
+                else if (c == 'a' || c == 'A')
                     input.movecursorleft();
-                else if (c == 's')
+                else if (c == 's' || c == 'S')
                     input.movecursordown();
-                else if (c == 'd')
+                else if (c == 'd' || c == 'D')
                     input.movecursorright();
                 else
                     std::cout << "Unknown command: " << c << std::endl;
